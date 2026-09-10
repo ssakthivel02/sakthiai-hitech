b042521839a66639b265b8739f70d82605f1bca4
ssakthivel02 <ssakthivel02@gmail.com>
2026-09-10 19:49:50 +0100

    creator: mount Creator workspace route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import Creator from "./pages/Creator";
 import Home from "./pages/Home";
 import IntelligenceDashboard from "./pages/IntelligenceDashboard";
 
@@ -11,6 +12,7 @@ function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/creator"} component={Creator} />
       <Route path={"/intelligence"} component={IntelligenceDashboard} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
